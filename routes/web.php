0dffe51cc6c9fdd3cc0add9d826c3ddad79743d3
DHP <phudhpc07544@fpt.edu.vn>
2024-07-23 09:26:49 +0700

    UPLOAD-[VIEW-PROFILE]

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('create_stock', function () {
 Route::get('edit_stock', function () {
     return view('stocks.edit');
 });
+
+Route::get('profile', function () {
+    return view('profiles.index');
+});
+
+Route::get('edit_profile', function() {
+    return view('profiles.edit');
+}); 
