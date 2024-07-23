08bca130cf0dcff9831dcea5faf14ccd6798314a
duyphuong010604 <voduyphuong13@gmail.com>
2024-07-23 08:52:49 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('products.show');
-});
 
-Route::get('stocks', function () {
-    return view('stocks.index');
-});
-Route::get('view_stock', function () {
-    return view('stocks.show');
-});
-
-Route::get('create_stock', function () {
-    return view('stocks.create');
-});
-
-Route::get('edit_stock', function () {
-    return view('stocks.edit');
+Route::middleware('web')->group(function () {
+    Route::get('/san-pham', [ProductController::class, 'index'])->name('san-pham.index');
+    Route::get('/san-pham/tao-moi', [ProductController::class, 'create'])->name('san-pham.create');
+    Route::post('/san-pham', [ProductController::class, 'store'])->name('san-pham.store');
+    Route::get('/san-pham/{id}', [ProductController::class, 'show'])->name('san-pham.show');
+    Route::get('/san-pham/{id}/chinh-sua', [ProductController::class, 'edit'])->name('san-pham.edit');
+    Route::put('/san-pham/{id}', [ProductController::class, 'update'])->name('san-pham.update');
+    Route::delete('/san-pham/{id}', [ProductController::class, 'destroy'])->name('san-pham.destroy');
 });
