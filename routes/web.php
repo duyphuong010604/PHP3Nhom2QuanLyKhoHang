fc518f2d54a581ed4ec72bd62856b9e73278058a
ThanhNhan <pronhan636@gmail.com>
2024-07-25 17:56:43 +0700

    [PULL FROM DEVELOP]

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-
 
 Route::get('/', function () {
-    return view('authentications.signIn');
+    return view('welcome');
 });
 
-require __DIR__ . "/dashboard.php";
-require __DIR__ . "/product.php";
-require __DIR__ . "/customer.php";
-require __DIR__ . "/inboundShipment.php";
-require __DIR__ . "/outboundShipment.php";
-require __DIR__ . "/stock.php";
-require __DIR__ . "/user.php";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
