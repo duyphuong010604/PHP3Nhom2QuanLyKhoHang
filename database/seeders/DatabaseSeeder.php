f6f7b038800c503088eb5fc0a2924d83ee129735
duyphuong010604 <voduyphuong13@gmail.com>
2024-08-06 00:15:10 +0700

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Customer;
 use App\Models\InboundShipment;
 use App\Models\InboundShipmentDetails;
 use App\Models\Product;
@@ -28,18 +29,19 @@ class DatabaseSeeder extends Seeder
                 // ]);
 
                 // Category::factory(5)->create();
-                Product::factory(10)->create();
+                // Product::factory(10)->create();
                 // Supplier::factory(10)->create();
-                Shelf::factory(10)->create();
+                // Shelf::factory(10)->create();
                 // InboundShipment::factory(10)->create();
                 // InboundShipmentDetails::factory(10)->create();
-                Stock::factory(10)->create();
+                // Stock::factory(10)->create();
                 // $this->call([
                 //     CategorySeeder::class,
                 //     ProductSeeder::class
                 // ]);
-                Category::factory(5)->create();
-                Product::factory(10)->create();
+                // Category::factory(5)->create();
+                // Product::factory(10)->create();
+                Customer::factory(10)->create();
                 // $this->call([
                 //     CategorySeeder::class,
                 //     ProductSeeder::class
