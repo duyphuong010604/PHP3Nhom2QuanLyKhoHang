d07387a8696596ba06f9bf9216c6e6607a6e8371
duyphuong010604 <voduyphuong13@gmail.com>
2024-08-07 17:41:27 +0700

    update

--- routes/outboundShipment.php
+++ routes/outboundShipment.php
@@ -1,20 +1,22 @@
 <?php
 
+use App\Http\Controllers\OutboundShipmentController;
 use Illuminate\Support\Facades\Route;
 
 use App\Livewire\OutboundShipments\Index;
 use App\Livewire\OutboundShipments\OutboundShipmentCreate;
 use App\Livewire\OutboundShipments\OutboundShipmentDetail;
 
-    
+
 Route::prefix('xuat-hang')->name('xuat-hang.')->group(function () {
-    Route::get('/', Index::class)->name('index'); 
-    Route::get('/tao-moi',OutboundShipmentCreate::class)->name('create');
-    Route::get('/{id}',OutboundShipmentDetail::class)->name('detail');
+    Route::get('/', Index::class)->name('index');
+    Route::get('/tao-moi', OutboundShipmentCreate::class)->name('create');
+    Route::get('/{id}', OutboundShipmentDetail::class)->name('detail');
 
     // Route::post('/', [OutboundShipmentController::class, 'store'])->name('store');
     // Route::get('/{id}', [OutboundShipmentController::class, 'show'])->name('show');
     // Route::get('/{id}/chinh-sua', [OutboundShipmentController::class, 'edit'])->name('edit');
+
     // Route::put('/{id}', [OutboundShipmentController::class, 'update'])->name('update');
     // Route::delete('/{id}', [OutboundShipmentController::class, 'destroy'])->name('destroy');
 });
