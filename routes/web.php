cd6df84feec8e3dda0f47ea23ace184423522ac3
duyphuong010604 <voduyphuong13@gmail.com>
2024-08-10 16:14:22 +0700

    mer giakhang

--- routes/web.php
+++ routes/web.php
@@ -70,10 +70,10 @@ Route::middleware(['auth'])->group(function () {
     });
 });
 require __DIR__ . '/auth.php';
-// require __DIR__ . "/dashboard.php";
-// require __DIR__ . "/product.php";
-// require __DIR__ . "/customer.php";
-// require __DIR__ . "/inboundShipment.php";
-// require __DIR__ . "/outboundShipment.php";
-// require __DIR__ . "/stock.php";
+require __DIR__ . "/dashboard.php";
+require __DIR__ . "/product.php";
+require __DIR__ . "/customer.php";
+require __DIR__ . "/inboundShipment.php";
+require __DIR__ . "/outboundShipment.php";
+require __DIR__ . "/stock.php";
 
